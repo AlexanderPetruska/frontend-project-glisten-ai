18c1f150febd6ab1ee50f68920f0c40831eea506
Sanikk <alexander.petruska@icloud.com>
2024-05-01 21:15:18 +0200

    Full Mobile NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,13 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { Content } from "@prismicio/client";
+import { Content, asLink } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import ButtonLink from "@/components/ButtonLink";
 import WordMark from "@/components/WordMark";
 import { MdMenu, MdClose } from "react-icons/md";
 import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 type NavBarProps = {
   settings: Content.SettingsDocument;
@@ -15,12 +16,19 @@ type NavBarProps = {
 
 export default function NavBar({ settings }: NavBarProps) {
   const [open, setOpen] = useState(false);
+  const pathName = usePathname();
 
   return (
     <nav className="px-4 py-4 md:px-6 md:py-6" aria-label="Main">
       <div className="mx-auto flex max-w-6xl flex-col justify-between gap-6 py-2 font-medium text-white sm:flex-row md:flex-row md:items-center">
         <div className="flex items-center justify-between">
-          <Link href={"/"}>
+          <Link
+            onClick={() => {
+              setOpen(false);
+            }}
+            className="z-50"
+            href={"/"}
+          >
             <WordMark />
             <span className="sr-only">Glisten.ai Home Page</span>
           </Link>
@@ -52,6 +60,42 @@ export default function NavBar({ settings }: NavBarProps) {
             <MdClose />
             <span className="sr-only">Close Menu</span>
           </button>
+
+          <div className="grid justify-items-end gap-8">
+            {settings.data.navigation.map((item) => {
+              if (item.cta_button) {
+                return (
+                  <ButtonLink
+                    key={item.label}
+                    field={item.link}
+                    onClick={() => setOpen(false)}
+                    aria-current={
+                      pathName.includes(asLink(item.link) as string)
+                        ? "page"
+                        : undefined
+                    }
+                  >
+                    {item.label}
+                  </ButtonLink>
+                );
+              }
+              return (
+                <PrismicNextLink
+                  key={item.label}
+                  className="block px-3 text-3xl first:mt-8"
+                  field={item.link}
+                  onClick={() => setOpen(false)}
+                  aria-current={
+                    pathName.includes(asLink(item.link) as string)
+                      ? "page"
+                      : undefined
+                  }
+                >
+                  {item.label}
+                </PrismicNextLink>
+              );
+            })}
+          </div>
         </div>
         {/* Desktop Nav */}
 
