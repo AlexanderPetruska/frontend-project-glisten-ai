3ab3fc56f0eb27fc018cf484b07e488148140112
Sanikk <alexander.petruska@icloud.com>
2024-05-06 14:25:07 +0200

    Adding animations

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,6 +4,7 @@ import { PrismicText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
 import ButtonLink from "@/components/ButtonLink";
 import StarGrid from "@/components/StarGird";
+import AnimatedContent from "./AnimatedContent";
 
 /**
  * Props for `Hero`.
@@ -20,33 +21,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="text-center"
     >
-      <div className="relative">
-        <StarGrid />
-        {isFilled.richText(slice.primary.heading) && (
-          <h1 className=" text-balance text-5xl font-medium md:text-7xl">
-            <PrismicText field={slice.primary.heading} />
-          </h1>
-        )}
-        {isFilled.richText(slice.primary.body) && (
-          <div className="mx-auto mb-8 mt-6 max-w-md text-balance text-slate-300">
-            <PrismicText field={slice.primary.body} />
-          </div>
-        )}
-        {isFilled.link(slice.primary.button_link) && (
-          <ButtonLink field={slice.primary.button_link}>
-            {slice.primary.button_label}
-          </ButtonLink>
-        )}
-        {isFilled.image(slice.primary.image) && (
-          <div className="glass-container mt-16">
-            <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter" />
-            <PrismicNextImage
-              className="rounded-lg "
-              field={slice.primary.image}
-            />
-          </div>
-        )}
-      </div>
+      <AnimatedContent slice={slice} />
     </Bounded>
   );
 };
